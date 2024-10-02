d58bbfc1801f3fc0a77c85ef1f39a7429f6d66c3
masarkar-tamu <masarker@tamu.edu>
2024-10-02 10:43:56 -0500

    added code and rspec tests for create_and_add_deploy_key function

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,6 +1,39 @@
 require 'octokit'
 
 module AssignmentsHelper
+    def create_and_add_deploy_key(repo_name)
+        # Define paths
+        key_path = './deploy_key'
+        secret_key_path = './secrets/deploy_key'
+        # Step 1: Generate SSH key
+        begin
+          Open3.capture3("ssh-keygen -t ed25519 -C \"gradescope\" -f #{key_path} -N ''")
+        rescue StandardError => e
+          puts "Failed to generate SSH key: #{e.message}"
+          return
+        end
+        begin
+          FileUtils.mv(key_path, secret_key_path)
+        rescue StandardError => e
+          puts "Failed to move deploy key: #{e.message}"
+          return
+        end
+        begin
+          public_key_content = File.read("#{key_path}.pub")
+        rescue StandardError => e
+          puts "Failed to read public key: #{e.message}"
+          return
+        end
+        begin
+          client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+          client.add_deploy_key(repo_name, "Gradescope Deploy Key", public_key_content, read_only: false)
+        rescue Octokit::Error => e
+          puts "Failed to add deploy key to GitHub: #{e.response_body[:message]}"
+          return
+        end
+        puts "Deploy key added successfully for #{repo_name}!"
+    end
+    
     def create_repo_from_template(assignment)
         client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
         template_repo = ENV['GITHUB_TEMPLATE_REPO_URL']
