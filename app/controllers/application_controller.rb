f75e4391e4fe3d81377e97ef3005033c588adf94
saksham1919 <saksham19@tamu.edu>
2024-10-04 15:31:55 -0500

    Adds Assignments Controller and Model code for updating remote repo feature.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,80 @@
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+class AssignmentsController < ApplicationController
+  before_action :set_assignment, only: %i[ show edit update destroy ]
+
+  # GET /assignments or /assignments.json
+  def index
+    @assignments = Assignment.all
+  end
+
+  # GET /assignments/1 or /assignments/1.json
+  def show
+  end
+
+  # GET /assignments/new
+  def new
+    @assignment = Assignment.new
+  end
+
+  # GET /assignments/1/edit
+  def edit
+  end
+
+  # POST /assignments or /assignments.json
+  def create
+    @assignment = Assignment.new(assignment_params)
+
+    respond_to do |format|
+      if @assignment.save
+        format.html { redirect_to @assignment, notice: "Assignment was successfully created." }
+        format.json { render :show, status: :created, location: @assignment }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @assignment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /assignments/1 or /assignments/1.json
+  def update
+    respond_to do |format|
+      if @assignment.update(assignment_params)
+        format.html { redirect_to @assignment, notice: "Assignment was successfully updated." }
+        format.json { render :show, status: :ok, location: @assignment }
+        current_user = User.find(session[:user_id]) # Retrieve the current user
+        auth_token = session[:github_token] # Get the GitHub auth token from the session
+        update_remote(current_user, auth_token)
+        redirect_to @assignment, notice: 'Assignment was successfully updated.'
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @assignment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update_remote(user, auth_token)
+    #1. Update code tests file
+    #2. Push changes to remote
+    @assignment.push_changes_to_github(user, auth_token)
+  end
+
+  # DELETE /assignments/1 or /assignments/1.json
+  def destroy
+    @assignment.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to assignments_path, status: :see_other, notice: "Assignment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_assignment
+      @assignment = Assignment.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def assignment_params
+      params.require(:assignment).permit(:assignment_name, :repository_name, :repository_url)
+    end
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,43 @@
-class Assignment < ActiveRecord::Base
-    has_many :tests
-end
+class Assignment < ApplicationRecord
+    # after_save is not needed anymore as we are manually calling this from the controller in the update action.
+    # Therefore, removed after_save :push_changes_to_github
+  
+    # Push any changes to the remote GitHub repository
+    def push_changes_to_github(user, auth_token)
+      # Retrieve the base path for all assignments from the environment variable
+      base_repo_path = ENV['ASSIGNMENTS_BASE_PATH'] # Base directory for all repositories
+      repository_name = self.repository_name # Repository name is a property of the assignment
+  
+      # Construct the local path to the specific assignment's repository
+      local_repo_path = File.join(base_repo_path, repository_name)
+  
+      # Construct the remote GitHub URL using the environment variable and the repository name
+      remote_repo_url = "https://#{auth_token}@github.com/#{ENV['GITHUB_COURSE_ORGANIZATION']}/#{repository_name}.git"
+  
+      # Set the remote URL for the repository
+      system("git -C #{local_repo_path} remote set-url origin #{remote_repo_url}")
+  
+      # Check if the local repo exists before committing and pushing changes
+      if Dir.exist?(local_repo_path)
+        commit_local_changes(local_repo_path, user)
+        sync_to_github(local_repo_path)
+      else
+        Rails.logger.error "Local repository not found for #{repository_name}"
+      end
+    end
+  
+    private
+  
+    # Commit local changes to the repository
+    def commit_local_changes(local_repo_path, user)
+      git = Git.open(local_repo_path) # Assuming the `ruby-git` gem is being used
+      git.add(all: true) # Add all changes (new, modified, deleted files)
+      git.commit("Changes made by #{user.username}") # Use the passed user object to get username
+    end
+  
+    # Push local changes to the GitHub repository
+    def sync_to_github(local_repo_path)
+      git = Git.open(local_repo_path)
+      git.push('origin', 'main') # Push to the remote repository's main branch
+    end
+end  
