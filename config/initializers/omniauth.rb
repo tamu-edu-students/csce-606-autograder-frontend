8ed5ef12151eb7d7acc8e532ffb85ca1d2a5cd4b
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-10-04 02:35:46 -0500

    rubocop fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],
+    provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"],
               scope: "user:email, read:org",
               client_options: {
                 site: "https://api.github.com",
