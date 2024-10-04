4ae7aa5e59f3da49fa43319b783a20de23795737
Banna150149 <mdhasanalbanna@tamu.edu>
2024-10-04 06:34:09 -0500

    added the validation for name, type, target and points

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,8 @@ class Test < ApplicationRecord
   belongs_to :assignment
   validates :actual_test, presence: true
   has_one_attached :target
+  validates :name, presence: true
+  validates :points, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :test_type, presence: true
+  validates :target, presence: true
 end
