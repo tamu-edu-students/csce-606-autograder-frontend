6afdcb7df2e63105f7773432f5ce492130d01c14
saksham1919 <saksham19@tamu.edu>
2024-10-16 11:43:14 -0500

    Adds step defs to validate clearing the search bar

--- features/step_definitions/search_bar_steps.rb
+++ features/step_definitions/search_bar_steps.rb
@@ -32,5 +32,16 @@
     expect(page).to have_content('No matching assignments found') # Ensure this message matches your app's output
   end
   
+  When('I clear the search bar') do
+    fill_in 'query', with: '' # Clears the search bar by entering an empty string
+    click_button 'Search Assignment'
+  end
+  
+  Then('I should see the full list of assignments') do
+    Assignment.all.each do |assignment|
+      expect(page).to have_content(assignment.repository_name)
+    end
+  end
+  
   
   
