9fa4a81d3192cdb15b41af0cf990f226f5147f08
yaoya2618-dot <yaoya2618@tamu.edu>
2024-10-17 12:44:56 -0500

    Cucumber tests for Create test group

--- app/models/test_grouping.rb
+++ app/models/test_grouping.rb
@@ -1,8 +1,11 @@
 class TestGrouping < ApplicationRecord
   belongs_to :assignment
   has_many :tests
-
+  
   acts_as_list scope: :assignment
+
+  validates :name, presence: { message: "Test grouping name can't be blank" }
+  validates :name, uniqueness: { scope: :assignment_id, message: "A test case grouping with this name already exists" }
 
   after_destroy :reassign_tests_to_default_grouping
 
