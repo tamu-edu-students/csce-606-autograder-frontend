c81cca15f305407ee41224d21eb52151e1f85868
gonzalezpear8 <gonzalezpear@tamu.edu>
2024-10-18 14:55:50 -0500

    fixed the failing rubocop issues

--- features/step_definitions/consistent_styling_steps.rb
+++ features/step_definitions/consistent_styling_steps.rb
@@ -2,7 +2,7 @@ require 'rspec/mocks'
 
   Then('primary navigation elements should be links styled as buttons with clear labels') do
     expect(page).to have_selector('a.btn') # This will check for elements with the class "btn"
-    
+
     page.all('.btn').each do |button|
       expect(button[:value].present? || button.text.present?).to be true
     end
