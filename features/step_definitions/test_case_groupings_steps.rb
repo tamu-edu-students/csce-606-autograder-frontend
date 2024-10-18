c5426780720109d1e1710121896e313a507f060c
maxsmith271346 <maxsmith271346@gmail.com>
2024-10-17 20:26:56 -0500

    Changes made by Max Smith

--- features/step_definitions/test_case_groupings_steps.rb
+++ features/step_definitions/test_case_groupings_steps.rb
@@ -1,6 +1,6 @@
-And("I should see a text field at the top of the scrollable block") do
-    expect(page).to have_css(".scrollable-container .new-test-grouping-form .form-control", visible: true)
-  end
+  And("I should see a text field at the top of the scrollable block") do
+      expect(page).to have_css(".scrollable-container .new-test-grouping-form .form-control", visible: true)
+    end
 
   And("I fill in the text field with {string}") do |grouping_name|
     fill_in "New Test Grouping", with: grouping_name
@@ -9,7 +9,7 @@ And("I should see a text field at the top of the scrollable block") do
   And("I click the add button") do
     find(".new-test-grouping-form .btn-primary").click
   end
-  
+
   Then("I should see {string} in the list of test case groupings") do |grouping_name|
     within(".scrollable-container") do
       expect(page).to have_content(grouping_name)
@@ -26,12 +26,103 @@ And("I should see a text field at the top of the scrollable block") do
 
   And("the following test case groupings exist for {string}:") do |assignment_name, table|
     assignment = Assignment.find_by(assignment_name: assignment_name)
-  
+
     table.hashes.each do |row|
         grouping_name = row["grouping_name"]
         TestGrouping.create!(name: grouping_name, assignment: assignment)
     end
   end
-  
-  
-  
+
+
+  When('I select {string} for the assignment {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I select {string} for the assignments {string} and {string}') do |string, string2, string3|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I select {string} for all assignments') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I deselect {string} for the assignment {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I deselect {string} for the assignments {string} and {string}') do |string, string2, string3|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I deselect {string} for all assignments') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then('I should see that {string} has {string} to the remote {string} repository') do |string, string2, string3|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I click on the {string} button') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I should see the Create test part on the right') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I click on the {string} grouping name') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I should see the list of test cases associated with {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I click on the {string} test case name') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I should see the test sub-number within grouping and other paras on the right') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I scroll down in test case grouping block') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I should see a button named {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then('I should be taken to the top of the test case groupings block') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Given('the {string} test case grouping exists') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I click on the {string} button next to {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I update the {string} to {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I attempt to update an existing test case grouping with the name {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  When('I confirm the deletion') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then('I should no longer see {string} in the list of test case groupings') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then('I should see a message {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
--- features/test_case_groupings.feature
+++ features/test_case_groupings.feature
@@ -8,7 +8,7 @@ Feature: Test Case Groupings CRUD
         | assignment_name | repository_name   |
         | assignment1     | assignment1 |
         | assignment2     | assignment2 |
-        | assignment3     | assignment3 | 
+        | assignment3     | assignment3 |
     Given I am logged in as an "instructor"
     And the following test case groupings exist for "assignment1":
       | grouping_name       |
@@ -25,7 +25,7 @@ Feature: Test Case Groupings CRUD
     And I click the add button
     Then I should see "Performance Tests" in the list of test case groupings
     And I should see a success message "Test case grouping 'Performance Tests' created successfully"
-  
+
   Scenario: Create duplicate test case grouping
     And I should see "Basic Functionality" in the list of test case groupings
     And I fill in the text field with "Basic Functionality"
@@ -37,51 +37,39 @@ Feature: Test Case Groupings CRUD
     And I click the add button
     Then I should see an error message "Test grouping name can't be blank"
 
+  Scenario: Create a new test case inside a test case grouping
+    When I click on the "Create New Test Case" button
+    And I should see the Create test part on the right
 
+  Scenario: View an existing test case grouping and test cases
+    When I click on the "Basic Functionality" grouping name
+    And I should see the list of test cases associated with "Basic Functionality"
+    When I click on the "test1" test case name
+    And I should see the test sub-number within grouping and other paras on the right
 
+  Scenario: Scroll to top button inside test case grouping block
+    When I scroll down in test case grouping block
+    And I should see a button named "Scroll to top"
+    And I click "Scroll to top"
+    Then I should be taken to the top of the test case groupings block
 
-  # Scenario: Create a new test case inside a test case grouping
-  #   When I click on the "Create New Test Case" button
-  #   And I should see the Create test part on the right
+  Scenario: Update an existing test case grouping name
+    Given the "Edge Cases" test case grouping exists
+    When I click on the "Edit" button next to "Edge Cases"
+    And I update the "Grouping Name" to "Boundary Tests"
+    And I click "Save"
+    Then I should see "Boundary Tests" in the list of test case groupings
+    And I should see a success message "Test case grouping 'Edge Cases' updated to 'Boundary Tests'"
 
-  # Scenario: View an existing test case grouping and test cases
-  #   When I click on the "Basic Functionality" grouping name
-  #   And I should see the list of test cases associated with "Basic Functionality"
-  #   When I click on the "test1" test case name
-  #   And I should see the test sub-number within grouping and other paras on the right
+  Scenario: Prevent duplicate test case grouping names in update
+    Given the "Basic Functionality" test case grouping exists
+    When I attempt to update an existing test case grouping with the name "Basic Functionality"
+    And I click "Submit"
+    Then I should see an error message "A test case grouping with this name already exists"
 
-  # Scenario: Scroll to top button inside test case grouping block
-  #   When I scroll down in test case grouping block
-  #   And I should see a button named "Scroll to top"
-  #   And I click "Scroll to top"
-  #   Then I should be taken to the top of the test case groupings block
-
-
-
-
-
-
-
-
-
-  # Scenario: Update an existing test case grouping name
-  #   Given the "Edge Cases" test case grouping exists
-  #   When I click on the "Edit" button next to "Edge Cases"
-  #   And I update the "Grouping Name" to "Boundary Tests"
-  #   And I click "Save"
-  #   Then I should see "Boundary Tests" in the list of test case groupings
-  #   And I should see a success message "Test case grouping 'Edge Cases' updated to 'Boundary Tests'"
-  
-  # Scenario: Prevent duplicate test case grouping names in update
-  #   Given the "Basic Functionality" test case grouping exists
-  #   When I attempt to update an existing test case grouping with the name "Basic Functionality"
-  #   And I click "Submit"
-  #   Then I should see an error message "A test case grouping with this name already exists"
-
-  # Scenario: Delete a test case grouping
-  #   Given the "Basic Functionality" test case grouping exists
-  #   When I click on the "Delete" button next to "Basic Functionality"
-  #   And I confirm the deletion
-  #   Then I should no longer see "Basic Functionality" in the list of test case groupings
-  #   And I should see a message "Test case grouping 'Basic Functionality' deleted successfully"
-
+  Scenario: Delete a test case grouping
+    Given the "Basic Functionality" test case grouping exists
+    When I click on the "Delete" button next to "Basic Functionality"
+    And I confirm the deletion
+    Then I should no longer see "Basic Functionality" in the list of test case groupings
+    And I should see a message "Test case grouping 'Basic Functionality' deleted successfully"
