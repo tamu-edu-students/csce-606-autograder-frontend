ae54a0f2d3d6829caaa0f51c0cafa49f27afeeef
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-10-19 21:36:48 -0500

    change regex to text

--- db/migrate/20241014224806_rename_number_to_position_in_tests_and_test_groupings_test.rb
+++ db/migrate/20241014224806_rename_number_to_position_in_tests_and_test_groupings_test.rb
@@ -1,7 +1,7 @@
 class RenameNumberToPositionInTestsAndTestGroupingsTest < ActiveRecord::Migration[7.2]
   def up
-    change_column :tests, :position, 'integer USING CASE WHEN position ~ E\'^\\d+$\' THEN position::integer ELSE 0 END'
-    change_column :test_groupings, :position, 'integer USING CASE WHEN position ~ E\'^\\d+$\' THEN position::integer ELSE 0 END'
+    change_column :tests, :position, 'integer USING CASE WHEN position::text ~ E\'^\\d+$\' THEN position::integer ELSE 0 END'
+    change_column :test_groupings, :position, 'integer USING CASE WHEN position::text ~ E\'^\\d+$\' THEN position::integer ELSE 0 END'
   end
 
   def down
