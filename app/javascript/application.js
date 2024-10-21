22c36476e977685917d8fec1b930523dacbc30e9
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-10-21 16:54:11 -0500

    keep the migration changes only

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,9 @@
+// app/javascript/application.js
+
 // Ensure Rails UJS is imported and started
 import Rails from "@rails/ujs";
 Rails.start();
 
 // Turbo and other modules
 import "@hotwired/turbo-rails";
-
-// Stimulus.js setup for controllers
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-
-// Start Stimulus application
-const application = Application.start();
-const context = require.context("controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
+import "controllers";
