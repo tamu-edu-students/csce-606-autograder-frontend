514da0969fec974956af91b407809a889d23d4d5
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-10-22 10:36:08 -0500

    Updated .gitignore and removed files from public/assets

--- public/assets/application-13840640afde850880724a364ec7fa6dfd19fc412d1ca6d36414d39b3f782ada.js
+++
@@ -1,9 +0,0 @@
-// app/javascript/application.js
-
-// Ensure Rails UJS is imported and started
-import Rails from "@rails/ujs";
-Rails.start();
-
-// Turbo and other modules
-import "@hotwired/turbo-rails";
-import "controllers";
