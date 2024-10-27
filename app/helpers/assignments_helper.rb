f7c9c90ded8b8e0fd01e7c49da0acc9fe21f8dc9
saksham1919 <saksham19@tamu.edu>
2024-10-27 18:19:08 -0500

    Adds logic for expandable Assignment directory dropdown functionality.

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,3 +1,22 @@
-
 module AssignmentsHelper
-end
+    def render_file_tree(tree_data)
+      content_tag(:ul, class: 'file-tree') do
+        tree_data.map do |node|
+          if node[:type] == 'directory'
+            content_tag(:li, class: 'folder') do
+              concat(
+                content_tag(:span, node[:name], class: 'folder-name', onclick: 'toggleFolder(this)')
+              )
+              concat(content_tag(:div, render_file_tree(node[:children]), class: 'children-container hidden')) if node[:children].present?
+            end
+          else
+            content_tag(:li, class: 'file') do
+                concat(
+                    content_tag(:span, node[:name], class: 'file-name')
+                )
+            end
+          end
+        end.join.html_safe
+      end
+    end
+  end
