f268e9fc5f8e5fb576d20b90142f9eb2c137f59b
Banna150149 <mdhasanalbanna@tamu.edu>
2024-10-29 19:53:40 -0500

    Added logic to assignments helper for file upload.

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,23 +1,28 @@
 module AssignmentsHelper
-    def render_file_tree(tree_data)
-      return '' unless tree_data.is_a?(Array)
-      content_tag(:ul, class: 'file-tree') do
-        tree_data.map do |node|
-          if node[:type] == 'directory'
-            content_tag(:li, class: 'folder') do
-              concat(
-                content_tag(:span, node[:name], class: 'folder-name', onclick: 'toggleFolder(this)')
-              )
-              concat(content_tag(:div, render_file_tree(node[:children]), class: 'children-container hidden')) if node[:children].present?
-            end
-          else
-            content_tag(:li, class: 'file') do
-                concat(
-                    content_tag(:span, node[:name], class: 'file-name')
-                )
-            end
+  def render_file_tree(tree_data, path = '')
+    return '' unless tree_data.is_a?(Array)
+    content_tag(:ul, class: 'file-tree') do
+      tree_data.map do |node|
+        full_path = path.present? ? "#{path}/#{node[:name]}" : node[:name]
+        if node[:type] == 'directory'
+          content_tag(:li, class: 'folder') do
+            concat(
+              content_tag(:span, node[:name], class: 'folder-name', onclick: 'toggleFolder(this)')
+            )
+            concat(
+              link_to '⬆️', '#', class: 'upload-icon', onclick: "openFileUpload(this, '#{full_path}')", title: 'Upload File'
+            )
+            concat(content_tag(:div, render_file_tree(node[:children], full_path), class: 'children-container hidden')) if node[:children].present?
           end
-        end.join.html_safe
-      end
-    end
+        else
+          content_tag(:li, class: 'file') do
+            concat(
+              content_tag(:span, node[:name], class: 'file-name')
+            )
+          end
+        end
+      end.join.html_safe
+    end +
+    content_tag(:input, nil, type: 'file', id: 'fileUploadInput', style: 'display: none;', onchange: 'uploadFile(this)')
   end
+end
