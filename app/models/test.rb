1d1b09c0c17bf454558c306d166b97aa5de34932
yaoya2618-dot <yaoya2618@tamu.edu>
2024-10-29 21:16:49 -0500

    first IO partial

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
 
   acts_as_list scope: :test_grouping
 
-  VALID_TEST_TYPES = [ "approved_includes", "compile", "coverage", "i/o", "memory_errors", "performance", "script", "unit"  ]
+  VALID_TEST_TYPES = [ "approved_includes", "compile", "coverage", "i_o", "memory_errors", "performance", "script", "unit"  ]
 
   # Validations for required fields
   validates :test_block, presence: true
@@ -45,7 +45,7 @@ class Test < ApplicationRecord
     when "unit", "performance"
       # code block with prepended tabs on each line
       test_block["code"].split("\n").map { |line| "\t#{line}" }.join("\n")
-    when "i/o"
+    when "i_o"
       # input and output paths with prepended tabs
       "\tinput: #{test_block["input_path"]}\n" \
       "\toutput: #{test_block["output_path"]}"
