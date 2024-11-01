f199336c44dc141c483bcf2f9c67b1dbb0c83331
yaoya2618-dot <yaoya2618@tamu.edu>
2024-11-01 09:32:27 -0500

    drag and drop inside group without changing backlog 'position'

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 import TestGroupingCardController from "../controllers/test_grouping_card_controller"
+import TestCardController from "../controllers/test_card_controller"
 import "../views/assignments/show"
 const application = Application.start()
 
 // Register the test_grouping_card controller
 application.register("test_grouping_card", TestGroupingCardController)
+application.register("test_card", TestCardController)
 
 // Configure Stimulus development experience
 application.debug = false
