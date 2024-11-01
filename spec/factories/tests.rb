b6e47dc212438e9c31f789011bfee943d4f7f86e
yaoya2618-dot <yaoya2618@tamu.edu>
2024-11-01 11:32:57 -0500

    add rspec for update order(drag and drop) and for render partial(dynamic test block)

---
+++ spec/factories/tests.rb
@@ -0,0 +1,14 @@
+# spec/factories/tests.rb
+FactoryBot.define do
+    factory :test do
+      association :assignment  # Ensures each test is associated with an assignment
+      name { "Sample Test" }
+      points { 10 }
+      test_type { "unit" }
+      position { 1 }
+      test_block { { "code" => "Sample code block" } }
+      target { "sample_target.cpp" }
+      # Add any other necessary attributes and associations here
+    end
+  end
+  
