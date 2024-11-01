86d632742f19bdb5c3713ed97ecf1d82da8075b9
masarkar-tamu <masarkar@tamu.edu>
2024-11-01 05:51:34 -0500

    fix rubocop issues

--- features/step_definitions/file_upload_steps.rb
+++ features/step_definitions/file_upload_steps.rb
@@ -19,7 +19,7 @@ end
 
 When("I select the Upload File button next to {string} folder in file tree") do |folder_name|
   upload_icon = find("li.folder", text: folder_name).find(".upload-icon")
-  
+
   # Click the upload icon to open the file upload dialog
   upload_icon.click
 end
@@ -34,7 +34,7 @@ When("I upload a new file {string} under the {string} folder") do |file_name, fo
       path: folder_name,
       id: @assignment.id
     }
-    
+
     @stub = stub_request(:put, "https://api.github.com/repos/AutograderFrontend/assignment1/contents/tests/#{folder_name}/#{file_name}").
     with(
       body: "{\"content\":\"VGVzdCBjb250ZW50IGZvciBmaWxlIHVwbG9hZC4=\",\"message\":\"Upload test_file.txt\"}",
@@ -49,11 +49,11 @@ When("I upload a new file {string} under the {string} folder") do |file_name, fo
     # Simulate the POST request to the upload_file action
     page.driver.post "/assignments/#{@assignment.id}/upload_file", params
 
-    #Checking the success response
+    # Checking the success response
     @upload_response = JSON.parse(page.driver.response.body)
 
     if @upload_response['success']
-      #Including the success message for later checks
+      # Including the success message for later checks
       allow_any_instance_of(AssignmentsHelper).to receive(:render_file_tree).and_return(
         " <h4>File uploaded successfully!</h4>
           <ul class='file-tree'>
@@ -69,9 +69,9 @@ When("I upload a new file {string} under the {string} folder") do |file_name, fo
 end
 
 Then("I should see the {string} file under the {string} folder in file directory") do |file_name, folder_name|
-  #Within the test setting, it is impossible to upload file to a remote repository as real HTTP interactions are disabled.
-  #So a good point to check whether the remote repository is updated, is to verify that the relevant GitHub API receives
-  #a call with the correct arguments.
-  
+  # Within the test setting, it is impossible to upload file to a remote repository as real HTTP interactions are disabled.
+  # So a good point to check whether the remote repository is updated, is to verify that the relevant GitHub API receives
+  # a call with the correct arguments.
+
   expect(@stub).to have_been_requested
 end
