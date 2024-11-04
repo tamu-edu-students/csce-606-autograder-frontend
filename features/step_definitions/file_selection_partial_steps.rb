027546724fb5bfe3dfb37ecd138967953f698635
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-11-03 20:09:33 -0600

    fix cucumber for file selection

--- features/step_definitions/file_selection_partial_steps.rb
+++ features/step_definitions/file_selection_partial_steps.rb
@@ -24,51 +24,36 @@ Then("I should see a nested file structure dropdown") do
   end
 end
 
-And('the dropdown should contain the following structure:') do |table|
-  puts page.html
-  within('#include-file-tree-dropdown') do
-    expect(page).to have_css('.file-tree', visible: true)
-    within ('.file-tree') do
-      table.rows.each do |directory, subdirectory, file|
-        if file.present?
-          expect(page).to have_css("li.file.file-item label", text: file)
-        elsif subdirectory.present?
-          expect(page).to have_css("li.dir.dir-item > span.directory-name", text: subdirectory)
-        else
-          expect(page).to have_css("li.dir.dir-item > span.directory-name", text: directory.split('/').last)
-        end
-      end
-    end
-  end
-end
-
 When("I expand the {string} directory") do |dir_name|
   within("#include-file-tree-dropdown") do
     find("li", text: dir_name).click
   end
 end
 
-And('I select the following files:') do |table|
-  within('#include-file-tree-dropdown') do
-    table.rows.each do |directory, file|
-      # Expand the directory if it's not already expanded
-      dir_element = find('span.directory-name', text: directory.split('/').last, visible: true)
-      dir_element.click
-      puts page.html
-      puts "#{directory.split('/').last}"
-      # Wait for the directory children to be visible
-      # expect(page).to have_xpath("//li[contains(@class, 'dir') and contains(@class, 'dir-item')][contains(., '#{directory.split('/').last}')]//ul[contains(@class, 'directory-children')]", visible: true)
-
-      # Find and check the checkbox for the file
-      within("li.dir.dir-item:contains('#{directory.split('/').last}') ul.directory-children", visible: true) do
-        checkbox = find("input.file-checkbox[data-file-path$='#{file}']")
-        checkbox.check
-      end
+And(/^I select the following files:$/) do |table|
+  table.hashes.each do |row|
+    directory = row['Directory']
+    file_name = row['File']
+    file_path = "#{directory}/#{file_name}"
+    within("#include-file-tree-dropdown") do
+      directory_element = find("span.directory-name[data-path='#{directory}']", visible: :all)
+      directory_element.click
+      expect(page).to have_css("ul.directory-children", visible: :all, wait: 10)
+      checkbox = find("input[type='checkbox'][data-file-path='#{file_path}']", visible: :all)
+      page.execute_script("arguments[0].checked = true; arguments[0].dispatchEvent(new Event('change', { bubbles: true }));", checkbox.native)
     end
   end
 end
 
-Then("the include field should display the selected file paths") do
-  selected_files = find("#test_include").value
-  expect(selected_files).to include("test_file1.cpp", "test_file2.cpp")
+
+Then(/^the include field should display the selected file paths$/) do
+  checkboxes = all("input[type='checkbox'][data-file-path]", visible: :all)
+  sleep 0.5
+
+  include_field = find("#test_include", visible: :all)
+
+  selected_file_paths = checkboxes.select(&:checked?).map { |checkbox| checkbox['data-file-path'] }
+  displayed_file_paths = include_field.value.split(",").map(&:strip)
+
+  expect(displayed_file_paths).to match_array(selected_file_paths)
 end
