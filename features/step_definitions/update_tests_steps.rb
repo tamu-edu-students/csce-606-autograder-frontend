727e17dc3ed0b77e6c94a6faf34e45823d2f23bd
yaoya2618-dot <yaoya2618@tamu.edu>
2024-11-12 16:36:37 -0600

    Test main cucumber

--- features/step_definitions/update_tests_steps.rb
+++ features/step_definitions/update_tests_steps.rb
@@ -22,6 +22,7 @@ Given(/^I am logged in as an instructor$/) do
     check_test_block(test_type)
     fill_test_block(test_type)
 
+    # Test main cucumber
     expect(page).to have_button('Create Test')
     expect(page).to have_select('Test Type', selected: test_type)
     click_button "Create Test"
