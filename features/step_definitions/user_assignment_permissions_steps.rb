a3009ee861587cfb84b480cc12c3083e379f4878
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-11-14 11:57:53 -0600

    fix cucumber steps

--- features/step_definitions/user_assignment_permissions_steps.rb
+++ features/step_definitions/user_assignment_permissions_steps.rb
@@ -35,8 +35,17 @@ end
 # Step definitions for user interactions
 When("I click on {string}") do |link|
   if link == "include"
-    find('#test_include').click
+    find('#include-file-dropdown').click
     expect(page).to have_css('#include-file-tree-dropdown', visible: true, wait: 5)
+  elsif link == "compile"
+    find('#test_block_compile_paths').click
+    expect(page).to have_css('#compile-file-tree-dropdown', visible: true, wait: 5)
+  elsif link == "source-paths"
+    find('#test_block_source_paths').click
+    expect(page).to have_css('#source-path-file-tree-dropdown', visible: true, wait: 5)
+  elsif link == "memory_errors"
+    find('#test_block_mem_error_paths').click
+    expect(page).to have_css('#mem-error-file-tree-dropdown', visible: true, wait: 5)
   else
     click_link link
   end
