0405f08920f0bb4c703ec2a483c2e1c31b66beae
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-11-16 15:12:47 -0600

    fix text block to JSON migration

--- db/migrate/20241027212147_rename_and_change_actual_test_in_tests.rb
+++ db/migrate/20241027212147_rename_and_change_actual_test_in_tests.rb
@@ -1,6 +1,6 @@
 class RenameAndChangeActualTestInTests < ActiveRecord::Migration[7.2]
   def change
-    rename_column :tests, :actual_test, :test_block
+    rename_column :tests, :actual_test, :test_block, using: 'test_block::jsonb'
     change_column :tests, :test_block, :jsonb, default: {}
   end
 end
