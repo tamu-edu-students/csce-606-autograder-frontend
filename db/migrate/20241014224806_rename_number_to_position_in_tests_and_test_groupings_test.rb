3b49a537f4fe45d918b3c3574ac56e67fedae366
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-11-16 16:03:11 -0600

    fix production migration

--- db/migrate/20241014224806_rename_number_to_position_in_tests_and_test_groupings_test.rb
+++ db/migrate/20241014224806_rename_number_to_position_in_tests_and_test_groupings_test.rb
@@ -1,6 +1,6 @@
 class RenameNumberToPositionInTestsAndTestGroupingsTest < ActiveRecord::Migration[7.2]
   def change
-    change_column :tests, :position, :integer
-    change_column :test_groupings, :position, :integer
+    change_column :tests, :position, :integer,  using: 'position::integer'
+    change_column :test_groupings, :position, :integer,  using: 'position::integer'
   end
 end
--- db/migrate/20241113201410_change_files_to_submit_to_assignments.rb
+++ db/migrate/20241113201410_change_files_to_submit_to_assignments.rb
@@ -1,5 +1,5 @@
 class ChangeFilesToSubmitToAssignments < ActiveRecord::Migration[7.2]
   def change
-    change_column :assignments, :files_to_submit, :jsonb, default: { "files_to_submit": [] }
+    change_column :assignments, :files_to_submit, :jsonb, using: 'files_to_submit::jsonb', default: { "files_to_submit": [] }
   end
 end
