cb00fcba4f4846b11c5d90b6c2b1636ac83cc339
Riddhi Ghate <riddhighate.07@tamu.edu>
2024-11-21 14:19:18 -0600

    conditional change_column in migrations

--- db/migrate/20241113201410_change_files_to_submit_to_assignments.rb
+++ db/migrate/20241113201410_change_files_to_submit_to_assignments.rb
@@ -1,5 +1,9 @@
 class ChangeFilesToSubmitToAssignments < ActiveRecord::Migration[7.2]
   def change
-    change_column :assignments, :files_to_submit, :jsonb, using: 'files_to_submit::jsonb', default: { "files_to_submit": [] }
+    if ActiveRecord::Base.connection.adapter_name.downcase == 'sqlite'
+      change_column :assignments, :files_to_submit, :jsonb, default: { "files_to_submit": [] }
+    else
+      change_column :assignments, :files_to_submit, :jsonb, using: 'files_to_submit::jsonb', default: { "files_to_submit": [] }
+    end
   end
 end
